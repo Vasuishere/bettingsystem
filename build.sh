d9f216a3994aa9767abe663c3cda84d73edd5529
vaibhav lohiya <vaibhav60080@gmail.com>
2025-11-13 19:01:45 +0530

    Add auto superuser creation command

--- build.sh
+++ build.sh
@@ -14,6 +14,9 @@ pip install -r requirements.txt
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+echo "Creating superuser..."
+python manage.py createsuperuser_auto
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
