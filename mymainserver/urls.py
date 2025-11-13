e352dd3b4aa0112e7ccc4a3b09214c62ea251c41
vaibhav lohiya <vaibhav60080@gmail.com>
2025-11-13 15:53:22 +0530

    Remove adminapp and related files from the project

--- mymainserver/urls.py
+++ mymainserver/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('userbaseapp.urls')),
-    path('adminapp/', include('adminapp.urls')),
 ]
